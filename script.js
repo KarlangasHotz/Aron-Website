7b605688f56c1ee01ef46598fcc99234acf6cb7a
Karlangas <karlhotzmolina@hotmail.com>
2022-01-01 13:27:08 +0100

    canvas+ black&white
    added canvas
    changed UI colors to BW

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-/* logic of a button / payment services*/
+/* Canvas ; hidden for now, soon to be given role in website
+*/
+
+const canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "seashell";
+
+ctx.fillRect(0, 0, 100, 100);
+
+ctx.moveTo(0, 0);
+
+ctx.stroke();
