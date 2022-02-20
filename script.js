23c41bc8a7f39cae87400e0462da876f6693cb12
Karlangas <karlhotzmolina@hotmail.com>
2022-02-20 12:48:43 +0100

    Finished Dark Mode & Button Animation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Canvas ; hidden for now, soon to be given role in website
-*/
+
 
 const canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
@@ -11,12 +11,13 @@ ctx.fillRect(0, 0, 100, 100);
 ctx.moveTo(0, 0);
 
 ctx.stroke();
+*/
 
 //targeting button
 let buttonnight = document.querySelector(".buttons");
 
 //styling it -why not
-buttonnight.style.backgroundColor = "black";
+buttonnight.style.backgroundColor = "rgb(36, 35, 35)";
 buttonnight.style.width = "10%";
 buttonnight.style.height = "10%";
 
@@ -25,26 +26,30 @@ function dark (){
 //when clicked:
 //starting DOM changes from bottom elements to top
 
-//footer.background goes black + its text color goes white
+//footer.background goes rgb(36, 35, 35) + its text color goes white
 let footer = document.querySelector("footer");
+let footerEmail = document.querySelector("footer a");
+
+console.log(footerEmail)
 
 footer.style.backgroundColor = "seashell";
-footer.style.color = "black";
+footer.style.color = "rgb(36, 35, 35)";
+footerEmail.style.color = "rgb(36, 35, 35)";
 
 
 
 
 // <figcaption> have to go color white
-let figcaption = document.querySelectorAll("a");
+let figcaption = document.querySelectorAll("figcaption a");
 
 figcaption.forEach(element => element.style.color = "seashell");
 
 
-//bottom background has to go black
+//bottom background has to go rgb(36, 35, 35)
 
 let bottom = document.querySelector(".bottom");
 
-bottom.style.backgroundColor = "black";
+bottom.style.backgroundColor = "rgb(36, 35, 35)";
 
 //bottom text has to go white
 
@@ -57,28 +62,28 @@ bottomText.forEach(element => element.style.color = "seashell");
 buttonnight.style.backgroundColor = "seashell";
 
 
-//button text go black
-buttonnight.style.color = "black";
+//button text go rgb(36, 35, 35)
+buttonnight.style.color = "rgb(36, 35, 35)";
 //button text change to "white mode"
 buttonnight.textContent = "White Mode"
 
-
-//canvas background black 
+/*
+//canvas background rgb(36, 35, 35) 
 let canvas = document.querySelector("canvas")
-canvas.style.backgroundColor = "black";
+canvas.style.backgroundColor = "rgb(36, 35, 35)";
+*/
 
 
-
-//class center background has to go black
+//class center background has to go rgb(36, 35, 35)
 let center = document.querySelectorAll(".center");
 
-center.forEach(element => element.style.backgroundColor = "black");
+center.forEach(element => element.style.backgroundColor = "rgb(36, 35, 35)");
 
 
 //class header has to go white backgound
 let header = document.querySelector(".header");
 
-header.style.backgroundColor = "white";
+header.style.backgroundColor = "seashell";
 
 }
 
@@ -88,25 +93,27 @@ function white (){
     //when clicked:
     //starting DOM changes from bottom elements to top
     
-    //footer.background goes black + its text color goes white
+    //footer.background goes rgb(36, 35, 35) + its text color goes white
     let footer = document.querySelector("footer");
+    let footerEmail = document.querySelector("footer a");
     
-    footer.style.backgroundColor = "black";
+    footerEmail.style.color = "seashell";
+    footer.style.backgroundColor = "rgb(36, 35, 35)";
     footer.style.color = "seashell";
     
     
     
     
     // <figcaption> have to go color white
-    let figcaption = document.querySelectorAll("a");
+    let figcaption = document.querySelectorAll("figcaption a");
     
-    figcaption.forEach(element => element.style.color = "black");
-    
-    
+    figcaption.forEach(element => element.style.color = "rgb(36, 35, 35)");
     
     
     
-    //bottom background has to go black
+    
+    
+    //bottom background has to go rgb(36, 35, 35)
     
     let bottom = document.querySelector(".bottom");
     
@@ -116,28 +123,29 @@ function white (){
     
     let bottomText = document.querySelectorAll(".bottom");
     
-    bottomText.forEach(element => element.style.color = "black");
+    bottomText.forEach(element => element.style.color = "rgb(36, 35, 35)");
     
     
     
     //button itself switching colors
     
-    buttonnight.style.backgroundColor = "black";
+    buttonnight.style.backgroundColor = "rgb(36, 35, 35)";
     
     
-    //button text go black
+    //button text go rgb(36, 35, 35)
     buttonnight.style.color = "seashell";
     //button text change to "white mode"
     buttonnight.textContent = "Dark Mode"
     
-    
-    //canvas background black 
+    /*
+    //canvas background rgb(36, 35, 35) 
     let canvas = document.querySelector("canvas")
     canvas.style.backgroundColor = "seashell";
     
+    */
     
     
-    //class center background has to go black
+    //class center background has to go rgb(36, 35, 35)
     let center = document.querySelectorAll(".center");
     
     center.forEach(element => element.style.backgroundColor = "seashell");
@@ -146,58 +154,40 @@ function white (){
     //class header has to go white backgound
     let header = document.querySelector(".header");
     
-    header.style.backgroundColor = "black";
+    header.style.backgroundColor = "rgb(36, 35, 35)";
 
     }
     
 
 
+
+
+
+
+
+
 //toggle button
+//dark() & white ()
+const buttonName = document.querySelector(".buttons")
 
+let counter = 1;
 
-
-function activateButton(button){
-    button.addEventListener("click", dark)
-}
-
-function disableButton(button){
-    button.removeEventListener("click", white );
-}
-
-
-function toggleButtons(value, button){
-
-    if (value === 1){
-        activateButton(button);
-    } else {
-        disableButton(button);
-
-    }
-};
-
-let clickCounter = 0;
-
-buttonnight.addEventListener("click", function (){
-
-
-    for (i = 0; i < 11; i++){
+let toggle = () =>{
+ 
+    if (counter % 2){
         
-        if (clickCounter  === 1|3|5|7|9){
-            toggleButtons(1, buttonnight)
-        }
-        else {
-            toggleButtons(0, buttonnight)
-
-        }
-        clickCounter += 1;
+        dark();
+        buttonName.textContent = "White Mode";
+    }else {
+        white();
+        buttonName.textContent = "Dark Mode";
     }
 
-})
-console.log(clickCounter)
+    counter++;
+}
 
-buttonnight.addEventListener("click", function (){
-    toggleButtons(0, buttonnight)
-})
+buttonName.addEventListener("click", toggle)
+
 
 
 
